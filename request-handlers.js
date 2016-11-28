91dfc6df430701f3f0b148735a2c8155ca1f809e
Frank Iriarte <jurrish@gmail.com>
2016-11-28 09:31:38 -0800

    node beginner

---
+++ request-handlers.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const exec = require('child_process').exec;
+
+function start() {
+  console.log('request handler START was called');
+  let content = 'empty';
+
+  exec('ls -lah', function(error, stdout, stderr) {
+    content = stdout;
+  });
+  return content;
+}
+
+function upload() {
+  console.log('request handler UPLOAD was called');
+  return 'hello upload';
+}
+
+exports.start = start;
+exports.upload = upload;
---
+++ server.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var url = require('url');
+
+// http.createServer(function(req, res) {
+//   res.writeHead(200, {"Content-Type": "text/plain"});
+//   res.write("Hello World");
+//   res.end();
+// }).listen(8888);
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log('request for ' + pathname + ' received');
+
+    route(handle, pathname);
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    var content = route(handle, pathname);
+    response.write(content);
+    response.end();
+  };
+
+  http.createServer(onRequest).listen(8888);
+
+  console.log('server has started');
+};
+
+exports.start = start;
